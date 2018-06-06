5b72dbbffcf5eb7d44a5b32db48971e921b8c1b3
Alexandra Mirtcheva <alex@solanalabs.io>
2018-06-06 13:58:14 -0700

    add linter config and add new component

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import React from 'react'
+import { Text, AppRegistry } from 'react-native'
 
-AppRegistry.registerComponent('albums', () => App);
+const App = () => {
+  return <Text>Some Text</Text>
+}
+
+AppRegistry.registerComponent('albums', () => App)
