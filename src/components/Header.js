10c814a001c604a6d3149506da34cb20c1443d51
Alexandra Mirtcheva <alex@solanalabs.io>
2018-06-11 08:32:59 -0700

    add view style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View } from 'react-native'
 
 const Header = () => {
-  const { textStyle } = styles
-  return <Text style={textStyle}>Albums</Text>
+  const { textStyle, viewStyle } = styles
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums</Text>
+    </View>
+  )
 }
 
 const styles = {
+  viewStyle: {
+    backgroundColor: '#f8f8f8',
+  },
   textStyle: {
     fontSize: 20,
   },
