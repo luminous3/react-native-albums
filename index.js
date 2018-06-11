45f5caf8dfdfa4bb78e0c26bd498e5bb66099c33
Alexandra Mirtcheva <alex@solanalabs.io>
2018-06-11 09:11:04 -0700

    new album list component

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import React from 'react'
-import { Text, AppRegistry } from 'react-native'
+import { AppRegistry, View } from 'react-native'
 import Header from './src/components/Header'
+import AlbumList from './src/components/AlbumList'
 
-const App = () => <Header headerText={'Albums'} />
+const App = () => (
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+)
 
 AppRegistry.registerComponent('albums', () => App)
