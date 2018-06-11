0ce3fc9d68b5c63b2797fe2057669461c4c4f90c
Alexandra Mirtcheva <alex@solanalabs.io>
2018-06-11 09:27:08 -0700

    fetch album data

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 
 class AlbumList extends Component {
+  //class level property
+  state = { albums: [] }
+
+  componentWillMount() {
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(res => res.json())
+      .then(resJson => {
+        this.setState({ albums: resJson })
+      })
+  }
+
   render() {
     return (
       <View>
